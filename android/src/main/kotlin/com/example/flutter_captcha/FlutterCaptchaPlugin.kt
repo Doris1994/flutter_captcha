ec184e597d314413f76062e5dcf435c880d4485b
zhengqingqing <zhengqingqing@geekbuy.com>
2021-09-06 09:16:42 +0800

    feat(/):添加Android端实现

--- android/src/main/kotlin/com/example/flutter_captcha/FlutterCaptchaPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_captcha/FlutterCaptchaPlugin.kt
@@ -1,35 +1,97 @@
 package com.example.flutter_captcha
 
 import androidx.annotation.NonNull
+import android.app.Activity
+import java.io.IOException
+import java.util.Map
+import java.lang.String
+
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import com.google.android.gms.safetynet.SafetyNet
+import com.google.android.gms.safetynet.SafetyNetApi
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.common.api.ApiException
+
 
 /** FlutterCaptchaPlugin */
-class FlutterCaptchaPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class FlutterCaptchaPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private lateinit var activity: Activity
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_captcha")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToActivity(activityPluginBinding: ActivityPluginBinding) {
+        activity = activityPluginBinding.getActivity();
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onDetachedFromActivityForConfigChanges() {
+        // TODO: the Activity your plugin was attached to was
+        // destroyed to change configuration.
+        // This call will be followed by onReattachedToActivityForConfigChanges().
+    }
+
+    override fun onReattachedToActivityForConfigChanges(activityPluginBinding: ActivityPluginBinding) {
+        onAttachedToActivity(activityPluginBinding);
+    }
+
+    override fun onDetachedFromActivity() {
+        // TODO: your plugin is no longer associated with an Activity.
+        // Clean up references.
+    }
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_captcha")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "validate") {
+            val siteKey: String? = call.argument("key")
+            SafetyNet.getClient(activity).verifyWithRecaptcha(siteKey!!.toString())
+                .addOnSuccessListener(activity, OnSuccessListener { response ->
+                    // Indicates communication with reCAPTCHA service was
+                    // successful.
+                    val userResponseToken = response.tokenResult
+                    if (response.tokenResult?.isNotEmpty() == true)
+                        result.success(response.getTokenResult())
+                    else
+                        result.error(
+                            "empty Token",
+                            "Verification using captcha has failed", null
+                        )
+
+                })
+                .addOnFailureListener(activity, OnFailureListener { e ->
+                    if (e is ApiException)
+                        result.error(
+                            e.statusCode.toString(),
+                            "Verification using captcha has failed", null
+                        )
+                    else
+                        result.error(
+                            e.message,
+                            "Verification using captcha has failed", null
+                        )
+
+                })
+
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
+
